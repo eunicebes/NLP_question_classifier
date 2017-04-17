a167bba34d6e20ae029c72889bf93e345882fccc
eunicebes <eunicebes@gmail.com>
2017-04-17 19:27:45 +0800

    Analysis dropout video

--- analysis/graphAPI.py
+++ analysis/graphAPI.py
@@ -18,3 +18,24 @@ def generate_bar_chart(input_list, chart_title):
     )
     fig = go.Figure(data=data, layout=layout)
     po.iplot(fig, filename='basic-bar')
+def generate_grouped_bar(input_list, chart_title):
+    data = []
+    for list_item in input_list:
+        x_axis = []
+        y_axis = []
+        label = list_item[1]
+        for item in list_item[0]:
+            x_axis.append(item[0])
+            y_axis.append(item[1])
+        trace = go.Bar(
+            x = x_axis,
+            y = y_axis,
+            name = label
+        )
+        data.append(trace)
+    layout = go.Layout(
+        title = chart_title,
+        barmode='group'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    po.iplot(fig, filename='grouped-bar')
